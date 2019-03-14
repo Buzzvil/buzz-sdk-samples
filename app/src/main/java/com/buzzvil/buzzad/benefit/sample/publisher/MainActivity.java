f4105a825d3298c656f20ff3b38716e4f819fc23
Josh Yun <josh.yun@buzzvil.com>
2019-03-14 18:40:43 +0900

    [BZZRWRDD-231] Interstitial Type Example (#10)
    * Version 1.3.5
    
    * InterstitialAd Examples

--- app/src/main/java/com/buzzvil/buzzad/benefit/sample/publisher/MainActivity.java
+++ app/src/main/java/com/buzzvil/buzzad/benefit/sample/publisher/MainActivity.java
@@ -1,17 +1,24 @@
 package com.buzzvil.buzzad.benefit.sample.publisher;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.buzzvil.buzzad.benefit.core.ad.AdError;
 import com.buzzvil.buzzad.benefit.presentation.feed.FeedConfig;
 import com.buzzvil.buzzad.benefit.presentation.feed.FeedHandler;
+import com.buzzvil.buzzad.benefit.presentation.interstitial.InterstitialAdConfig;
+import com.buzzvil.buzzad.benefit.presentation.interstitial.InterstitialAdHandler;
+import com.buzzvil.buzzad.benefit.presentation.interstitial.InterstitialAdHandlerFactory;
 import com.buzzvil.buzzad.benefit.presentation.nativead.NativeAd;
 import com.buzzvil.buzzad.benefit.presentation.nativead.NativeAdLoader;
 import com.buzzvil.buzzad.benefit.sample.publisher.feed.CustomAdsAdapter;
@@ -24,10 +31,13 @@ import java.util.Collection;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button interstitialAdButton;
+    private Button nativeAdButton;
     private Button nativeAdsButton;
     private Button feedButton;
     private ProgressBar progressBar;
+    private Spinner interstitialTypeSpinner;
+    private Spinner interstitialCustomizationSpinner;
+    private Button interstitialAdButton;
 
     private View adView;
 
@@ -36,15 +46,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        interstitialAdButton = findViewById(R.id.interstitial_ad_button);
-        interstitialAdButton.setOnClickListener(new View.OnClickListener() {
+        this.nativeAdButton = findViewById(R.id.native_ad_button);
+        nativeAdButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 loadNativeAd();
             }
         });
 
-        nativeAdsButton = findViewById(R.id.native_ads_button);
+        this.nativeAdsButton = findViewById(R.id.native_ads_button);
         nativeAdsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        feedButton = findViewById(R.id.feed_button);
+        this.feedButton = findViewById(R.id.feed_button);
         feedButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,7 +76,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        progressBar = findViewById(R.id.progress_bar);
+        this.progressBar = findViewById(R.id.progress_bar);
+
+        this.interstitialTypeSpinner = findViewById(R.id.type_spinner);
+        this.interstitialCustomizationSpinner = findViewById(R.id.customization_spinner);
+        this.interstitialAdButton = findViewById(R.id.interstitial_ad_button);
+        interstitialAdButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showInterstitialAd();
+            }
+        });
     }
 
     private void loadNativeAd() {
@@ -83,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
             public void onAdLoaded(@NonNull NativeAd nativeAd) {
                 progressBar.setVisibility(View.GONE);
 
-                InterstitialAdView interstitialAdView = new InterstitialAdView(MainActivity.this);
+                final InterstitialAdView interstitialAdView = new InterstitialAdView(MainActivity.this);
                 interstitialAdView.setOnCloseClickListener(new InterstitialAdView.OnCloseClickListener() {
                     @Override
                     public void onCloseClick() {
@@ -111,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
             public void onAdsLoaded(@NonNull Collection<NativeAd> collection) {
                 progressBar.setVisibility(View.GONE);
 
-                PagerAdsView pagerAdsView = new PagerAdsView(MainActivity.this);
+                final PagerAdsView pagerAdsView = new PagerAdsView(MainActivity.this);
                 pagerAdsView.setOnCloseClickListener(new PagerAdsView.OnCloseClickListener() {
                     @Override
                     public void onCloseClick() {
@@ -140,5 +160,80 @@ public class MainActivity extends AppCompatActivity {
         if (!closeAdView()) {
             super.onBackPressed();
         }
+    }
+
+    public void showInterstitialAd() {
+        final int selectedType = interstitialTypeSpinner.getSelectedItemPosition();
+
+        final InterstitialAdHandler.Type interstitialType = selectedType == 0 || selectedType == Spinner.INVALID_POSITION
+                ? InterstitialAdHandler.Type.Dialog
+                : InterstitialAdHandler.Type.BottomSheet;
+
+        final InterstitialAdHandler interstitialAdHandler = new InterstitialAdHandlerFactory().create(App.UNIT_ID_NATIVE_AD, interstitialType);
+
+        final int customizationType = interstitialCustomizationSpinner.getSelectedItemPosition();
+        switch (customizationType) {
+            case 1:
+                interstitialAdHandler.show(this,
+                        new InterstitialAdConfig.Builder()
+                                .topIcon(R.drawable.ic_gift_green)
+//                                .topIcon(ContextCompat.getDrawable(this, R.drawable.ic_gift_green))
+                                .build());
+                return;
+            case 2:
+                interstitialAdHandler.show(this,
+                        new InterstitialAdConfig.Builder()
+                                .layoutBackgroundColor(android.R.color.background_dark)
+                                .textColor(android.R.color.white)
+                                .titleText(getString(R.string.bz_interstitial_title))
+                                .closeText(getString(R.string.bz_interstitial_close)) // For Dialog only
+                                .build());
+                return;
+            case 3:
+                interstitialAdHandler.show(this,
+                        new InterstitialAdConfig.Builder()
+                                .ctaViewBackgroundColorList(getCtaViewBackgroundColorList())
+                                .ctaViewTextColor(getCtaViewTextColorList())
+                                .ctaRewardDrawable(R.drawable.ic_reward_gray)
+                                .ctaParticipatedDrawable(R.drawable.ic_participated)
+                                .build());
+                return;
+            case 0:
+            default:
+                // Default Dialog or BottomSheet does not have title and close button text.
+                // It is recommended to provide the strings with translations.
+                interstitialAdHandler.show(this);
+                break;
+        }
+    }
+
+    private ColorStateList getCtaViewBackgroundColorList() {
+        final int[][] states = new int[][]{
+                new int[]{android.R.attr.state_enabled},
+                new int[]{android.R.attr.state_pressed}
+        };
+
+        final int[] colors = new int[]{
+                ContextCompat.getColor(this, android.R.color.holo_blue_light),  // enabled
+                ContextCompat.getColor(this, android.R.color.holo_blue_dark)   // pressed
+        };
+
+        return new ColorStateList(states, colors);
+
+    }
+
+    private ColorStateList getCtaViewTextColorList() {
+        int[][] states = new int[][]{
+                new int[]{android.R.attr.state_enabled},
+                new int[]{android.R.attr.state_pressed}
+        };
+
+        int[] colors = new int[]{
+                Color.WHITE,  // enabled
+                Color.WHITE   // pressed
+        };
+
+        return new ColorStateList(states, colors);
+
     }
 }
