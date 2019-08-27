bbee3fad71ffb43d817ab135990d75c32341f907
Dio Seung Heo <elondhs@gmail.com>
2019-08-27 15:08:16 +0900

    buzzstore-sdk 1.0.8

--- buzzstore-sample/src/main/java/com/buzzvil/buzzstore/sample/MainActivity.java
+++ buzzstore-sample/src/main/java/com/buzzvil/buzzstore/sample/MainActivity.java
@@ -27,26 +27,26 @@ public class MainActivity extends Activity {
         BuzzStore.setUserTokenListener(new BuzzStore.UserTokenListener() {
             @Override
             public void OnNeedAPICall() {
-                ** 해당 함수가 호출되면 퍼블리셔 서버로의 API 콜을 통해 해당 유저의 유저토큰을 전달받아 setUserToken 메소드를 통해 등록해야 한다.
-                 * 보안상의 이유로 해당 유저토큰은 Server-To-Server로만 제공되므로 퍼블리셔앱은 반드시 퍼블리셔 서버를 통해서 버즈스토어로 요청해야 한다.
-                 * 주의 : 아래의 코드는 참고를 위한 Pseudo-code 로 실제로 이와 같은 메소드가 제공되지는 않는다.
-                 */
-                Request(
-                        new ResponseListener() {
-                            OnSuccess(String userToken) {
-                                /**
-                                 * 서버로부터 userToken을 전달받은 후에 아래의 메소드를 호출한다.
-                                 */
-                                BuzzStore.setUserToken(userToken);
-                            }
-                            OnError() {
-                                /**
-                                 * 서버 통신 실패 시 아래와 같이 빈 스트링으로 setUserToken을 다시 요청한다.
-                                 */
-                                BuzzStore.setUserToken("");
-                            }
-                        }
-                );
+                // ** 해당 함수가 호출되면 퍼블리셔 서버로의 API 콜을 통해 해당 유저의 유저토큰을 전달받아 setUserToken 메소드를 통해 등록해야 한다.
+                //  * 보안상의 이유로 해당 유저토큰은 Server-To-Server로만 제공되므로 퍼블리셔앱은 반드시 퍼블리셔 서버를 통해서 버즈스토어로 요청해야 한다.
+                //  * 주의 : 아래의 코드는 참고를 위한 Pseudo-code 로 실제로 이와 같은 메소드가 제공되지는 않는다.
+                //  */
+                // Request(
+                //         new ResponseListener() {
+                //             OnSuccess(String userToken) {
+                //                 /**
+                //                  * 서버로부터 userToken을 전달받은 후에 아래의 메소드를 호출한다.
+                //                  */
+                //                 BuzzStore.setUserToken(userToken);
+                //             }
+                //             OnError() {
+                //                 /**
+                //                  * 서버 통신 실패 시 아래와 같이 빈 스트링으로 setUserToken을 다시 요청한다.
+                //                  */
+                //                 BuzzStore.setUserToken("");
+                //             }
+                //         }
+                // );
             }
 
             @Override
