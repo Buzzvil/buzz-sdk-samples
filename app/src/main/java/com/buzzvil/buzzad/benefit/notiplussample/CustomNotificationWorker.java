ebe019227dc57defdb9a1f6cef8952738ee20b35
Bruce Kim <bruce.kim@buzzvil.com>
2020-01-20 18:42:57 +0900

    Add missing options

--- app/src/main/java/com/buzzvil/buzzad/benefit/notiplussample/CustomNotificationWorker.java
+++ app/src/main/java/com/buzzvil/buzzad/benefit/notiplussample/CustomNotificationWorker.java
@@ -7,8 +7,10 @@ import androidx.work.WorkerParameters;
 
 import com.buzzvil.buzzad.benefit.presentation.feed.FeedActivity;
 import com.buzzvil.buzzad.benefit.presentation.feed.FeedConfig;
+import com.buzzvil.buzzad.benefit.presentation.notification.NotiPlusCtaActivity;
 import com.buzzvil.buzzad.benefit.presentation.notification.NotificationConfig;
 import com.buzzvil.buzzad.benefit.presentation.notification.NotificationWorker;
+import com.buzzvil.buzzad.benefit.presentation.notification.RewardNotificationConfig;
 
 public class CustomNotificationWorker extends NotificationWorker {
     public CustomNotificationWorker(@NonNull Context context,
@@ -20,8 +22,10 @@ public class CustomNotificationWorker extends NotificationWorker {
     @NonNull
     public NotificationConfig getNotificationConfig() {
         final FeedConfig feedConfig = new FeedConfig.Builder(App.UNIT_ID_NOTI_PLUS).build();
+        final RewardNotificationConfig rewardNotificationConfig = new RewardNotificationConfig.Builder().build();
         return new NotificationConfig.Builder(App.UNIT_ID_NOTI_PLUS)
                 .putExtra(FeedActivity.EXTRA_CONFIG, feedConfig)
+                .putExtra(NotiPlusCtaActivity.EXTRA_REWARD_NOTIFICATION_CONFIG, rewardNotificationConfig)
                 .build();
     }
 }
