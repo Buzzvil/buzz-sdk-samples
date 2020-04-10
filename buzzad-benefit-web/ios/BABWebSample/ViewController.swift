2354c76887a5730e316ec399730cf1992a1f0050
Jaehee Ko <celestarry@gmail.com>
2020-04-10 17:37:16 +0900

    Create project

---
+++ buzzad-benefit-web/ios/BABWebSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BABWebSample
+//
+//  Created by Jaehee Ko on 10/04/2020.
+//  Copyright © 2020 Buzzvil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
