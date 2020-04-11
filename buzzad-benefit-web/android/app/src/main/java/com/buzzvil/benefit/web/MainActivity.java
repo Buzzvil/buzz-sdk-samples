8314684b96d37425caccd25e04e1a148473d7aae
ybbarng <ybbarng@gmail.com>
2020-04-11 15:56:20 +0900

    앱 시작시 랜덤으로 아이디 생성

--- buzzad-benefit-web/android/app/src/main/java/com/buzzvil/benefit/web/MainActivity.java
+++ buzzad-benefit-web/android/app/src/main/java/com/buzzvil/benefit/web/MainActivity.java
@@ -1,8 +1,13 @@
 package com.buzzvil.benefit.web;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.webkit.WebView;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,25 +16,49 @@ import com.buzzvil.buzzad.benefit.BuzzAdBenefit;
 import com.buzzvil.buzzad.benefit.core.js.BuzzAdBenefitJavascriptInterface;
 import com.buzzvil.buzzad.benefit.core.models.UserProfile;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
+    private EditText userIdView;
     private ToggleButton loginButton;
     private WebView webView;
+
+    private Random random;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.userIdView = findViewById(R.id.userid);
         this.loginButton = findViewById(R.id.login);
         this.webView = findViewById(R.id.webview);
+
+        this.random = new Random();
 
         setupView();
     }
 
     private void setupView() {
+        setupUserIdView();
         setupLoginButton();
         setupWebView();
+        final boolean isLogin = fetchBuzzvilCredential();
+        if (isLogin) {
+            openWebPage();
+        }
+    }
+
+    private void setupUserIdView() {
+        userIdView.setText("TEST_" + (random.nextInt() & Integer.MAX_VALUE));
+        BuzzAdBenefit.registerSessionReadyBroadcastReceiver(this, new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                BuzzAdBenefit.unregisterSessionReadyBroadcastReceiver(context, this);
+                fetchBuzzvilCredential();
+            }
+        });
     }
 
     private void setupLoginButton() {
@@ -51,25 +80,48 @@ public class MainActivity extends AppCompatActivity {
         webView.addJavascriptInterface(javascriptInterface, BuzzAdBenefitJavascriptInterface.INTERFACE_NAME);
     }
 
+    private void setLoginUi(final String userId) {
+        this.userIdView.setText(userId);
+        this.loginButton.setChecked(true);
+        this.userIdView.setEnabled(false);
+    }
+
+    private void setLogoutUi() {
+        this.loginButton.setChecked(false);
+        this.userIdView.setEnabled(true);
+    }
+
+    private boolean fetchBuzzvilCredential() {
+        final UserProfile userProfile = BuzzAdBenefit.getUserProfile();
+        if (userProfile != null && !TextUtils.isEmpty(userProfile.getUserId())) {
+            setLoginUi(userProfile.getUserId());
+            return true;
+        }
+        return false;
+    }
+
     private void login() {
         // Simulate a user's login
+        final String userId = userIdView.getText().toString();
+        if (TextUtils.isEmpty(userId)) {
+            this.setLogoutUi();
+            return;
+        }
         final UserProfile.Builder builder = new UserProfile.Builder(BuzzAdBenefit.getUserProfile());
         final UserProfile userProfile = builder
-                .userId("Your_Service_User_ID")
+                .userId(userId)
                 .gender(UserProfile.Gender.MALE)
                 .birthYear(1985)
                 .build();
 
         BuzzAdBenefit.setUserProfile(userProfile);
-
-        this.loginButton.setChecked(true);
+        this.setLoginUi(userId);
         openWebPage();
     }
 
     private void logout() {
         BuzzAdBenefit.setUserProfile(null);
-
-        this.loginButton.setChecked(false);
+        setLogoutUi();
         closeWebPage();
     }
 
