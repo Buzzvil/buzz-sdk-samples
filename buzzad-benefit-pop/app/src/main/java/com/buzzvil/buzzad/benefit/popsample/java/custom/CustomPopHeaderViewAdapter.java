a059617961d26330dd03edd54b16ca71359f54eb
Bruce Kim <bruce.kim@buzzvil.com>
2020-09-10 17:47:20 +0900

    Update customPreviewMessage name

--- buzzad-benefit-pop/app/src/main/java/com/buzzvil/buzzad/benefit/popsample/java/custom/CustomPopHeaderViewAdapter.java
+++ buzzad-benefit-pop/app/src/main/java/com/buzzvil/buzzad/benefit/popsample/java/custom/CustomPopHeaderViewAdapter.java
@@ -9,26 +9,26 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 
 import com.buzzvil.buzzad.benefit.pop.DefaultPopHeaderViewAdapter;
-import com.buzzvil.buzzad.benefit.pop.domain.model.MessagePreview;
+import com.buzzvil.buzzad.benefit.pop.domain.model.CustomPreviewMessage;
 import com.buzzvil.buzzad.benefit.popsample.R;
 
 public class CustomPopHeaderViewAdapter extends DefaultPopHeaderViewAdapter {
     @Nullable
     @Override
-    protected View getMessagePreviewLayout(Context context, ViewGroup parent, MessagePreview messagePreview) {
+    protected View getCustomPreviewMessageLayout(Context context, ViewGroup parent, CustomPreviewMessage customPreviewMessage) {
         final LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         if (inflater == null) {
             return null;
         }
-        View viewMessagePreview = inflater.inflate(R.layout.view_custom_preview_message, parent, false);
-        final TextView messagePreviewText = viewMessagePreview.findViewById(R.id.textCustomPreviewMessageTitle);
-        messagePreviewText.setText(messagePreview.getMessage());
-        viewMessagePreview.setOnClickListener(new View.OnClickListener() {
+        View viewCustomPreviewMessage = inflater.inflate(R.layout.view_custom_preview_message, parent, false);
+        final TextView textCustomPreviewMessageTitle = viewCustomPreviewMessage.findViewById(R.id.textCustomPreviewMessageTitle);
+        textCustomPreviewMessageTitle.setText(customPreviewMessage.getMessage());
+        viewCustomPreviewMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startDeeplinkActivity(context, messagePreview.getLandingUrl());
+                startDeeplinkActivity(context, customPreviewMessage.getLandingUrl());
             }
         });
-        return viewMessagePreview;
+        return viewCustomPreviewMessage;
     }
 }
