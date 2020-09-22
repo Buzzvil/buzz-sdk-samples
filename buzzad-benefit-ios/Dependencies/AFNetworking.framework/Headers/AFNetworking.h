07c546e204a35abb86781187457c7515a5f9318e
jongsu253 <jongsu253@gmail.com>
2020-09-22 11:48:58 +0900

    Add Dependencies

--- buzzad-benefit-ios/Dependencies/AFNetworking.framework/Headers/AFNetworking.h
+++ buzzad-benefit-ios/Dependencies/AFNetworking.framework/Headers/AFNetworking.h
@@ -1,16 +1,17 @@
 // AFNetworking.h
-// Copyright (c) 2011–2016 Alamofire Software Foundation ( http://alamofire.org/ )
 //
+// Copyright (c) 2013 AFNetworking (http://afnetworking.com/)
+// 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-//
+// 
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-//
+// 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,47 +21,21 @@
 // THE SOFTWARE.
 
 #import <Foundation/Foundation.h>
-
-//! Project version number for AFNetworking.
-FOUNDATION_EXPORT double AFNetworkingVersionNumber;
-
-//! Project version string for AFNetworking.
-FOUNDATION_EXPORT const unsigned char AFNetworkingVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <AFNetworking/PublicHeader.h>
-
 #import <Availability.h>
 #import <TargetConditionals.h>
 
 #ifndef _AFNETWORKING_
-#define _AFNETWORKING_
+    #define _AFNETWORKING_
 
-#import <AFNetworking/AFURLRequestSerialization.h>
-#import <AFNetworking/AFURLResponseSerialization.h>
-#import <AFNetworking/AFSecurityPolicy.h>
-#import <AFNetworking/AFCompatibilityMacros.h>
+    #import "AFURLRequestSerialization.h"
+    #import "AFURLResponseSerialization.h"
+    #import "AFSecurityPolicy.h"
 
 #if !TARGET_OS_WATCH
-#import <AFNetworking/AFNetworkReachabilityManager.h>
+    #import "AFNetworkReachabilityManager.h"
 #endif
 
-#import <AFNetworking/AFURLSessionManager.h>
-#import <AFNetworking/AFHTTPSessionManager.h>
-
-#if TARGET_OS_IOS || TARGET_OS_TV
-#import <AFNetworking/AFAutoPurgingImageCache.h>
-#import <AFNetworking/AFImageDownloader.h>
-#import <AFNetworking/UIActivityIndicatorView+AFNetworking.h>
-#import <AFNetworking/UIButton+AFNetworking.h>
-#import <AFNetworking/UIImageView+AFNetworking.h>
-#import <AFNetworking/UIProgressView+AFNetworking.h>
-#endif
-
-#if TARGET_OS_IOS
-#import <AFNetworking/AFNetworkActivityIndicatorManager.h>
-#import <AFNetworking/UIRefreshControl+AFNetworking.h>
-#import <AFNetworking/WKWebView+AFNetworking.h>
-#endif
-
+    #import "AFURLSessionManager.h"
+    #import "AFHTTPSessionManager.h"
 
 #endif /* _AFNETWORKING_ */
