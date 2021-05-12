08512f4b2528a8af78e4c37fe1074e84c1262761
ybbarng <ybbarng@gmail.com>
2021-05-12 17:43:44 +0900

    iOS도 새 탭에서 여는 링크만 외부 브라우저로 열리게 함

--- webfeed/ios/WebFeed Sample/WebFeed Sample/WebFeedViewController.swift
+++ webfeed/ios/WebFeed Sample/WebFeed Sample/WebFeedViewController.swift
@@ -17,10 +17,11 @@ class WebFeedViewController: UIViewController {
         // Your APP ID
         let appId = "123";
 
-        let url = "https://webfeed.buzzvil.com/app/\(appId)"
+        let url = "https://webfeed.buzzvil.com/\(appId)/feed"
         self.load(url: url);
 
         self.webView.allowsBackForwardNavigationGestures = true;
+        self.webView.uiDelegate = self;
     }
 
     func load(url: String) {
@@ -38,4 +39,13 @@ class WebFeedViewController: UIViewController {
             self.webView.goForward();
         }
     }
+}
+
+extension WebFeedViewController : WKUIDelegate {
+  func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+    if let url = navigationAction.request.url {
+      UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+    return nil;
+  }
 }
