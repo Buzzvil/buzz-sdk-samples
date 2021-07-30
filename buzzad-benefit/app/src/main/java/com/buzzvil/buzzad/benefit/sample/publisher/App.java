72d9fbb88294054bb14b1537f031c0ad67fcf57d
WesJeong <wes.jeong@buzzvil.com>
2021-07-30 18:52:18 +0900

    crashable code

--- buzzad-benefit/app/src/main/java/com/buzzvil/buzzad/benefit/sample/publisher/App.java
+++ buzzad-benefit/app/src/main/java/com/buzzvil/buzzad/benefit/sample/publisher/App.java
@@ -7,10 +7,12 @@ import androidx.multidex.MultiDex;
 import com.buzzvil.buzzad.benefit.BuzzAdBenefit;
 import com.buzzvil.buzzad.benefit.BuzzAdBenefitConfig;
 import com.buzzvil.buzzad.benefit.core.models.UserProfile;
+import com.buzzvil.buzzad.benefit.pop.PopConfig;
+import com.buzzvil.buzzad.benefit.presentation.feed.FeedConfig;
 
 public class App extends Application {
-    public static final String UNIT_ID_NATIVE_AD = "YOUR_NATIVE_AD_UNIT_ID";
-    public static final String UNIT_ID_FEED = "YOUR_FEED_UNIT_ID";
+    public static final String UNIT_ID_NATIVE_AD = "100000043";
+    public static final String UNIT_ID_FEED = "100000043";
 
     @Override
     protected void attachBaseContext(Context base) {
@@ -22,7 +24,10 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
 
-        BuzzAdBenefit.init(this, new BuzzAdBenefitConfig.Builder(this).build());
+        FeedConfig feedConfig = new FeedConfig.Builder(this, UNIT_ID_FEED).build();
+        PopConfig popConfig = new PopConfig.Builder(this, UNIT_ID_FEED).build();
+
+        BuzzAdBenefit.init(this, new BuzzAdBenefitConfig.Builder(this).setFeedConfig(feedConfig).setPopConfig(popConfig).build());
 
         final UserProfile userProfile = new UserProfile.Builder(BuzzAdBenefit.getUserProfile())
                 .userId("SAMPLE_USER_ID")
