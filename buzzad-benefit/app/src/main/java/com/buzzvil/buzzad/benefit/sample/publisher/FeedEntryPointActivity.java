b609fd21dcb508b71681343bc4188ec0a6a5c129
ybbarng <ybbarng@gmail.com>
2021-11-11 10:43:16 +0900

    Preload의 결과로 포인트 총합을 받아서 메세지에 표시 구현

--- buzzad-benefit/app/src/main/java/com/buzzvil/buzzad/benefit/sample/publisher/FeedEntryPointActivity.java
+++ buzzad-benefit/app/src/main/java/com/buzzvil/buzzad/benefit/sample/publisher/FeedEntryPointActivity.java
@@ -11,12 +11,16 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.buzzvil.buzzad.benefit.core.ad.AdError;
+import com.buzzvil.buzzad.benefit.presentation.feed.FeedHandler;
+
 import java.text.DecimalFormat;
 
 public class FeedEntryPointActivity extends AppCompatActivity {
 
     private TextView iconWithMessageImageView;
     private Button button;
+    private FeedHandler feedHandler;
 
     public static void startActivity(Context context) {
         final Intent intent = new Intent(context, FeedEntryPointActivity.class);
@@ -30,28 +34,54 @@ public class FeedEntryPointActivity extends AppCompatActivity {
 
         iconWithMessageImageView = findViewById(R.id.textViewIconWithMessage);
         button = findViewById(R.id.button);
+
+        feedHandler = new FeedHandler(this, App.UNIT_ID_FEED);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        updateMessages(12345);
+        feedHandler.preload(new FeedHandler.FeedPreloadListener() {
+            @Override
+            public void onPreloaded() {
+                updateMessages(feedHandler.getTotalReward());
+            }
+
+            @Override
+            public void onError(AdError adError) {
+                updateMessages(0);
+            }
+        });
     }
 
     private void updateMessages(final int points) {
-        DecimalFormat pointsFormat = new DecimalFormat("#,###");
-        updateMessages(pointsFormat.format(points));
-
+        if (points > 0) {
+            updateMessagesWithPoint(points);
+        } else {
+            updateMessagesWithoutPoint();
+        }
     }
 
-    private void updateMessages(final String points) {
+    private void updateMessagesWithPoint(final int points) {
+        final DecimalFormat pointsFormat = new DecimalFormat("#,###");
+        final String pointsString = pointsFormat.format(points);
         if (iconWithMessageImageView != null) {
-            final Spanned coloredMessage = Html.fromHtml(getString(R.string.feed_entry_point_message_with_highlight, points));
+            final Spanned coloredMessage = Html.fromHtml(getString(R.string.feed_entry_point_message_with_highlight, pointsString));
             iconWithMessageImageView.setText(coloredMessage);
         }
         if (button != null) {
-            button.setText(getString(R.string.feed_entry_point_message, points));
+            button.setText(getString(R.string.feed_entry_point_message, pointsString));
+        }
+    }
+
+    private void updateMessagesWithoutPoint() {
+        final String message = getString(R.string.feed_entry_point_message_without_points);
+        if (iconWithMessageImageView != null) {
+            iconWithMessageImageView.setText(message);
+        }
+        if (button != null) {
+            button.setText(getString(R.string.feed_entry_point_message, message));
         }
     }
 }
