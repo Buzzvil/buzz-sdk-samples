a31de5f314716ad80b9863e017fb502953db956c
Harry Park <harry.park@buzzvil.com>
2022-02-08 16:42:47 +0900

    Clean up Web to Feed code

--- buzzad-benefit-ios/BABSample/WebToFeed/WebToFeedViewController.m
+++ buzzad-benefit-ios/BABSample/WebToFeed/WebToFeedViewController.m
@@ -13,21 +13,25 @@ static NSString * const kNavigationItemTitle = @"Web to Feed";
 
 @implementation WebToFeedViewController
 
-- (void)viewDidLoad {
-  [super viewDidLoad];
-  
-  self.navigationItem.title = kNavigationItemTitle;
-  
+- (void)loadView {
   WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
   WKUserContentController *contentController = [[WKUserContentController alloc] init];
   [contentController addScriptMessageHandler:self name:@"BuzzAdBenefitJSBridge"];
   config.userContentController = contentController;
 
-  _webView = [[WKWebView alloc] initWithFrame:self.view.bounds configuration:config];
+  _webView = [[WKWebView alloc] initWithFrame:CGRectZero configuration:config];
+  _webView.allowsBackForwardNavigationGestures = YES;
+  self.view = _webView;
+}
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  
+  self.navigationItem.title = kNavigationItemTitle;
+  
   NSURL *url = [[NSBundle mainBundle] URLForResource:@"WebToFeedSample" withExtension:@"html"];
   NSString *html = [NSString stringWithContentsOfURL:url encoding:NSUTF8StringEncoding error:nil];
   [_webView loadHTMLString:html baseURL:nil];
-  [self.view addSubview:_webView];
 }
 
 - (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message {
