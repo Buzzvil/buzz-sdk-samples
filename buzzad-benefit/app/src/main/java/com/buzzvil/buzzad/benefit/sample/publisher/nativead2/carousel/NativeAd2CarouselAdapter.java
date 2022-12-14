998bdf0ef33c4d73b20a01c4c2dffe6d11b3b27e
Yeji Moon (Nike) <yeaji21@gmail.com>
2022-12-14 17:51:55 +0900

    Revert "remove carousel"
    This reverts commit 69352a0b71d9f873c8eedc08a22bd3c3b940b398.

---
+++ buzzad-benefit/app/src/main/java/com/buzzvil/buzzad/benefit/sample/publisher/nativead2/carousel/NativeAd2CarouselAdapter.java
@@ -0,0 +1,75 @@
+package com.buzzvil.buzzad.benefit.sample.publisher.nativead2.carousel;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.buzzvil.buzzad.benefit.nativead2.api.NativeAd2Pool;
+import com.buzzvil.buzzad.benefit.nativead2.api.NativeAd2View;
+import com.buzzvil.buzzad.benefit.sample.publisher.R;
+
+import java.util.List;
+
+public class NativeAd2CarouselAdapter extends RecyclerView.Adapter<NativeAd2CarouselViewHolder> {
+    private String unitId;
+    private List<NativeAd2CarouselItem> list;
+    private NativeAd2Pool carouselPool;
+    private Boolean infiniteLoop;
+
+    public NativeAd2CarouselAdapter(String unitId, List<NativeAd2CarouselItem> list, NativeAd2Pool carouselPool, Boolean infiniteLoop) {
+        this.unitId = unitId;
+        this.list = list;
+        this.carouselPool = carouselPool;
+        this.infiniteLoop = infiniteLoop;
+    }
+
+    @NonNull
+    @Override
+    public NativeAd2CarouselViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        NativeAd2View nativeAd2View = (NativeAd2View) LayoutInflater.from(parent.getContext()).inflate(R.layout.view_native_ad2_item, parent, false);
+        return new NativeAd2CarouselViewHolder(unitId, nativeAd2View);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NativeAd2CarouselViewHolder holder, int position) {
+        int itemPosition;
+        if (infiniteLoop) {
+            itemPosition = position % list.size();
+        } else {
+            itemPosition = position;
+        }
+
+        // 해당 position에 해당하는 NativeAd2ViewBinder가 carouselPool을 사용하도록 합니다.
+        holder.setPool(carouselPool, itemPosition);
+
+        NativeAd2CarouselItem item = list.get(itemPosition);
+
+        if (item instanceof NativeAd2CarouselItem.NativeAd2Item) {
+            holder.bind(itemPosition);
+        } else if (item instanceof NativeAd2CarouselItem.CarouselToFeedSlideItem) {
+            holder.bind(itemPosition, ((NativeAd2CarouselItem.CarouselToFeedSlideItem) item).feedPromotion);
+        }
+    }
+
+    @Override
+    public void onViewRecycled(NativeAd2CarouselViewHolder holder) {
+        int itemPosition = holder.getAdapterPosition() % list.size();
+        holder.unbind(itemPosition);
+
+        super.onViewRecycled(holder);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        int actualItemCount = list.size();
+        if (infiniteLoop && actualItemCount > 0) {
+            // 무한 루프를 쉽게 구현하는 방법으로 매우 큰 수를 여기서 반환한다.
+            return Integer.MAX_VALUE;
+        }
+        return actualItemCount;
+    }
+
+}
---
+++ buzzad-benefit/app/src/main/java/com/buzzvil/buzzad/benefit/sample/publisher/nativead2/carousel/NativeAd2CarouselViewHolder.java
@@ -0,0 +1,141 @@
+package com.buzzvil.buzzad.benefit.sample.publisher.nativead2.carousel;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.buzzvil.buzzad.benefit.core.ad.AdError;
+import com.buzzvil.buzzad.benefit.nativead2.api.NativeAd2;
+import com.buzzvil.buzzad.benefit.nativead2.api.NativeAd2EventListener;
+import com.buzzvil.buzzad.benefit.nativead2.api.NativeAd2Pool;
+import com.buzzvil.buzzad.benefit.nativead2.api.NativeAd2StateChangedListener;
+import com.buzzvil.buzzad.benefit.nativead2.api.NativeAd2View;
+import com.buzzvil.buzzad.benefit.nativead2.api.NativeAd2ViewBinder;
+import com.buzzvil.buzzad.benefit.presentation.feed.entrypoint.FeedPromotion;
+import com.buzzvil.buzzad.benefit.presentation.feed.entrypoint.FeedPromotionViewBinder;
+import com.buzzvil.buzzad.benefit.presentation.media.DefaultCtaView;
+import com.buzzvil.buzzad.benefit.presentation.media.MediaView;
+import com.buzzvil.buzzad.benefit.presentation.reward.RewardResult;
+import com.buzzvil.buzzad.benefit.sample.publisher.R;
+
+public class NativeAd2CarouselViewHolder extends RecyclerView.ViewHolder {
+
+    private TextView positionTextView;
+    private LinearLayout loadingView;
+
+    private NativeAd2ViewBinder nativeAd2ViewBinder;
+    private FeedPromotionViewBinder carouselToFeedViewBinder;
+
+    NativeAd2CarouselViewHolder(String unitId, NativeAd2View nativeAd2View) {
+        super(nativeAd2View);
+
+        positionTextView = nativeAd2View.findViewById(R.id.positionTextView);
+        TextView stateTextView = nativeAd2View.findViewById(R.id.stateTextView);
+        TextView eventTextView = nativeAd2View.findViewById(R.id.eventTextView);
+        this.loadingView = nativeAd2View.findViewById(R.id.loadingView);
+
+        MediaView mediaView = nativeAd2View.findViewById(R.id.mediaView);
+        ImageView imageIcon = nativeAd2View.findViewById(R.id.imageIcon);
+        TextView textTitle = nativeAd2View.findViewById(R.id.textTitle);
+        TextView textDescription = nativeAd2View.findViewById(R.id.textDescription);
+        DefaultCtaView ctaView = nativeAd2View.findViewById(R.id.ctaView);
+
+        nativeAd2ViewBinder = new NativeAd2ViewBinder.Builder()
+                .nativeAd2View(nativeAd2View)
+                .mediaView(mediaView)
+                .iconImageView(imageIcon)
+                .titleTextView(textTitle)
+                .descriptionTextView(textDescription)
+                .ctaView(ctaView)
+                .build(unitId);
+
+        nativeAd2ViewBinder.addNativeAd2StateChangedListener(new NativeAd2StateChangedListener() {
+            @Override
+            public void onRequested() {
+                stateTextView.setText("state: onRequested");
+            }
+
+            @Override
+            public void onNext(@NonNull NativeAd2 nativeAd2) {
+                stateTextView.setText("state: onNext");
+            }
+
+            @Override
+            public void onComplete() {
+                stateTextView.setText("state: onComplete");
+            }
+
+            @Override
+            public void onError(@NonNull AdError adError) {
+                stateTextView.setText("state: onError(" + adError.getAdErrorType().name() + ")");
+            }
+        });
+
+        nativeAd2ViewBinder.addNativeAd2EventListener(new NativeAd2EventListener() {
+            @Override
+            public void onImpressed(@NonNull NativeAd2 nativeAd2) {
+                eventTextView.setText("event: onImpressed");
+            }
+
+            @Override
+            public void onClicked(@NonNull NativeAd2 nativeAd2) {
+                eventTextView.setText("event: onClicked");
+            }
+
+            @Override
+            public void onRewardRequested(@NonNull NativeAd2 nativeAd2) {
+                eventTextView.setText("event: onRewardRequested");
+            }
+
+            @Override
+            public void onRewarded(@NonNull NativeAd2 nativeAd2, @NonNull RewardResult rewardResult) {
+                eventTextView.setText("event: onRewarded, result: " + rewardResult.name());
+            }
+
+            @Override
+            public void onParticipated(@NonNull NativeAd2 nativeAd2) {
+                eventTextView.setText("event: onParticipated");
+            }
+        });
+
+        carouselToFeedViewBinder = new FeedPromotionViewBinder.Builder(nativeAd2View, mediaView)
+                .titleTextView(textTitle)
+                .descriptionTextView(textDescription)
+                .iconImageView(imageIcon)
+                .ctaView(ctaView)
+                .build();
+    }
+
+    public void setPool(NativeAd2Pool carouselPool, int adKey) {
+        nativeAd2ViewBinder.setPool(adKey, carouselPool);
+    }
+
+    public void bind(int position) {
+        positionTextView.setText("position: " + position);
+        setLoadingView(false);
+        nativeAd2ViewBinder.bind(position);
+    }
+
+    public void bind(int position, FeedPromotion feedPromotion) {
+        positionTextView.setText("position: " + position);
+        setLoadingView(false);
+        carouselToFeedViewBinder.bind(feedPromotion);
+    }
+
+    public void unbind(int position) {
+        nativeAd2ViewBinder.unbind(position);
+        carouselToFeedViewBinder.unbind();
+    }
+
+    private void setLoadingView(boolean isLoading) {
+        if (isLoading) {
+            loadingView.setVisibility(View.VISIBLE);
+        } else {
+            loadingView.setVisibility(View.GONE);
+        }
+    }
+}
