a510edbe2d323fc3660b629f78df92bb14020203
Aaron Ahyeon Kim <73379813+dkghazz@users.noreply.github.com>
2023-04-05 11:17:09 +0900

    BuzzBooster Refine react-native sample app (#136)
    BuzzBooster Refine react-native sample app (#136)

--- buzzbooster-react-native/Page/EventView.js
+++ buzzbooster-react-native/Page/EventView.js
@@ -1,62 +1,46 @@
-import {
-  Text,
-  TextInput,
-  Button
-} from 'react-native';
+import { Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import React from 'react';
 import styles from '../styles';
 import { BuzzBooster } from 'react-native-buzz-booster';
-
-const EventTextInput = ({ setEventKey, setEventValue }) => {
-  return (
-    <SafeAreaView style={styles.rowContainer}>
-      <TextInput
-        style={styles.input}
-        onChangeText={setEventKey}
-        placeholder='key'
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={setEventValue}
-        placeholder='value'
-      />
-    </SafeAreaView >
-  );
-};
+import { Toast } from 'react-native-toast-message/lib/src/Toast';
 
 export default () => {
-  const [eventName, setEventName] = React.useState('');
-  const [eventKey1, setEventKey1] = React.useState('');
-  const [eventValue1, setEventValue1] = React.useState('');
-  const [eventKey2, setEventKey2] = React.useState('');
-  const [eventValue2, setEventValue2] = React.useState('');
-  const [eventKey3, setEventKey3] = React.useState('');
-  const [eventValue3, setEventValue3] = React.useState('');
   return (
     <SafeAreaView style={styles.columnContainer}>
-      <Text style={styles.title}>Event</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={setEventName}
-        placeholder='Input Event Name'
-      />
-      <EventTextInput setEventKey={setEventKey1} setEventValue={setEventValue1} />
-      <EventTextInput setEventKey={setEventKey2} setEventValue={setEventValue2} />
-      <EventTextInput setEventKey={setEventKey3} setEventValue={setEventValue3} />
-      <Button title='Send' onPress={() => {
-        let eventValues = {}
-        if (eventKey1.length !== 0 && eventValue1.length !== 0) {
-          eventValues[eventKey1] = eventValue1
-        }
-        if (eventKey2.length !== 0 && eventValue2.length !== 0) {
-          eventValues[eventKey2] = eventValue2
-        }
-        if (eventKey3.length !== 0 && eventValue3.length !== 0) {
-          eventValues[eventKey3] = eventValue3
-        }
-        BuzzBooster.sendEvent(eventName, eventValues)
-      }} />
+      <Button title='Like' onPress={like} />
+      <Button title='Comment' onPress={comment} />
+      <Button title='Post' onPress={post} />
     </SafeAreaView >
   );
+}
+
+function like() {
+  BuzzBooster.sendEvent('bb_like', { 'liked_content_id': 'post_1' });
+  Toast.show({
+    type: 'success',
+    text1: 'Like',
+    text2: 'You liked post_1',
+    position: 'bottom',
+  });
+}
+
+function comment() {
+  BuzzBooster.sendEvent('bb_comment', { 'commented_content_id': 'post_2', 'comment': 'Great Post!' });
+  Toast.show({
+    type: 'success',
+    text1: 'Comment',
+    text2: 'You commented post_2 with "Great Post!"',
+    position: 'bottom',
+  });
+}
+
+function post() {
+  BuzzBooster.sendEvent('bb_posting_content', { 'posted_content_id': 'post_3' });
+  Toast.show({
+    type: 'success',
+    text1: 'Post',
+    text2: 'You posted post_3',
+    position: 'bottom',
+  });
 }
--- buzzbooster-react-native/Page/OptInMarketingPage.js
+++ buzzbooster-react-native/Page/OptInMarketingPage.js
@@ -1,13 +1,26 @@
-import * as React from 'react';
-import { StyleSheet, ScrollView, Button, Text } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, ScrollView, Button, Text, Switch } from 'react-native';
+import { BuzzBooster } from 'react-native-buzz-booster';
 
 export default function OptInMarketingPage({ route, navigation }) {
+    const [isEnabled, setIsEnabled] = useState(false);
+    const toggleSwitch = () => {
+        setIsEnabled(previousState => !previousState)
+        BuzzBooster.sendEvent('bb_opt_in_marketing');
+    };
     const onPress = () => {
         navigation.navigate("HomePage")
     }
     return (
         <ScrollView style={styles.container}>
-            <Text>{route.params?.url}</Text>
+            <Text>{"수신동의 하시겠습니까?"}</Text>
+            <Switch
+                trackColor={{ false: '#767577', true: '#81b0ff' }}
+                thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+                ios_backgroundColor="#3e3e3e"
+                onValueChange={toggleSwitch}
+                value={isEnabled}
+            />
             <Button title="back" onPress={onPress} />
         </ScrollView>
     );
