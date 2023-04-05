959bad29557e213309b478b81ee27be3559b3409
Aaron Kim <dkghazz@gmail.com>
2023-04-05 11:05:59 +0900

    Refine sample app

--- buzzbooster-react-native/Page/HomePage.js
+++ buzzbooster-react-native/Page/HomePage.js
@@ -7,53 +7,72 @@ import {
   Button,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Colors, Header } from 'react-native/Libraries/NewAppScreen';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 import EventView from './EventView';
+import uuid from 'react-native-uuid';
+import { Toast } from 'react-native-toast-message/lib/src/Toast';
 
 export default function HomePage({ route, navigation }) {
-    const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === 'dark';
 
-    const backgroundStyle = {
-      backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-      flex: 1,
-    };
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1,
+  };
 
-    React.useEffect(() => {
-      async function setup() {
-          BuzzBooster.setCustomCampaignActionButtonClickListener((url) => {
-              navigation.navigate("LinkPage", { url: url })
-          })
-          BuzzBooster.setOptInMarketingCampaignMoveButtonClickListener(() => {
-              navigation.navigate("LinkPage", { url: "opt-in-marketing" })
-          })
-      }
-      setup()
+  React.useEffect(() => {
+    async function setup() {
+      BuzzBooster.setCustomCampaignActionButtonClickListener((url) => {
+        navigation.navigate("LinkPage", { url: url })
+      })
+      BuzzBooster.setOptInMarketingCampaignMoveButtonClickListener(() => {
+        navigation.navigate("OptInMarketingPage")
+      })
+    }
+    setup()
   }, []);
 
-    return (
-        <SafeAreaView style={backgroundStyle}>
-          <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-          <ScrollView
-            contentInsetAdjustmentBehavior='automatic'
-            style={backgroundStyle}>
-            <Header />
-            <Button title='Login' onPress={() => {
-              const userId = 'Damon1'
-              let user = new UserBuilder(userId)
-                .setOptInMarketing(true)
-                .addProperty("login_type", "sns(Facebook)")
-                .build()
-              BuzzBooster.setUser(user)
-              BuzzBooster.showInAppMessage();
-            }} />
-            <Button title='Campaign' onPress={() => {
-              BuzzBooster.showCampaign();
-            }} />
-            <Button title='Attendance Campaign' onPress={() => {
-              BuzzBooster.showSpecificCampaign(CampaignType.Attendance);
-            }} />
-            <EventView />
-          </ScrollView>
-        </SafeAreaView>
-      );
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior='automatic'
+        style={backgroundStyle}>
+        <Button title='Login' onPress={login} />
+        <Button title='InAppMessage' onPress={showInAppMessage} />
+        <Button title='Campaign' onPress={showCampaign} />
+        <Button title='Attendance Campaign' onPress={showAttendanceCampaign} />
+        <EventView />
+      </ScrollView>
+      <Toast />
+    </SafeAreaView>
+  );
+}
+
+function login() {
+  const userId = uuid.v4();
+  let user = new UserBuilder(userId)
+    .setOptInMarketing(true)
+    .addProperty("login_type", "sns(Facebook)")
+    .build()
+  BuzzBooster.setUser(user)
+
+  Toast.show({
+    type: 'success',
+    text1: 'Login Success',
+    text2: 'User Id: ' + userId,
+    position: 'bottom',
+  })
+}
+
+function showInAppMessage() {
+  BuzzBooster.showInAppMessage();
+}
+
+function showCampaign() {
+  BuzzBooster.showCampaign();
+}
+
+function showAttendanceCampaign() {
+  BuzzBooster.showSpecificCampaign(CampaignType.Attendance);
 }
