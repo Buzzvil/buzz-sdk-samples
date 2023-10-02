0c1c16fdc0f1d19a2e5a997d04d6e6b29b3cd44f
Yeji Moon (Nike) <yeaji21@gmail.com>
2023-10-02 21:56:56 +0900

    remove buzzvil code

--- buzzvil-sdk/app/src/main/java/com/buzzvil/sample/buzzvilsdksample/MainActivity.kt
+++ buzzvil-sdk/app/src/main/java/com/buzzvil/sample/buzzvilsdksample/MainActivity.kt
@@ -3,7 +3,6 @@ package com.buzzvil.sample.buzzvilsdksample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.buzzvil.sample.buzzvilsdksample.databinding.ActivityMainBinding
-import com.buzzvil.sdk.BuzzvilSdk
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -12,23 +11,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.loginButton.setOnClickListener {
-            // 사용자 정보를 등록하는 코드입니다.
-            BuzzvilSdk.setUserProfile(
-                userId = "SAMPLE_USER_ID",
-                gender = BuzzvilSdk.Gender.MALE,
-                birthYear = 1980,
-            )
-        }
-
-        binding.logoutButton.setOnClickListener {
-            // 사용자 정보를 삭제합니다.
-            BuzzvilSdk.setUserProfile(null)
-        }
-
-        binding.showBenefitHubButton.setOnClickListener {
-            BuzzvilSdk.showFeed(this@MainActivity)
-        }
     }
 }
