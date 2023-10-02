6662e8ec2d259b30ae393c8b6c81a538fb4f9a74
Yeji Moon (Nike) <yeaji21@gmail.com>
2023-10-02 22:00:48 +0900

    Change Gradle to use Groovy in BuzzvilSdkSample (#170)
    * Show BenefitHub
    
    * revert rootProject.name
    
    * remove buzzvil code
    
    * remove buzzvil code

--- buzzvil-sdk/app/src/main/java/com/buzzvil/sample/buzzvilsdksample/MainActivity.kt
+++ buzzvil-sdk/app/src/main/java/com/buzzvil/sample/buzzvilsdksample/MainActivity.kt
@@ -2,10 +2,14 @@ package com.buzzvil.sample.buzzvilsdksample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.buzzvil.sample.buzzvilsdksample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
