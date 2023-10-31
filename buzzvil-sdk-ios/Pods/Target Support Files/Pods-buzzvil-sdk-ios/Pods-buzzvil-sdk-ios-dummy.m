17e12aca23813572bf1e6bbb181d96eee74832d8
JuHyang <ljh28891004@gmail.com>
2023-10-31 15:49:32 +0900

    init buzzvil-sdk-ios project

---
+++ buzzvil-sdk-ios/Pods/Target Support Files/Pods-buzzvil-sdk-ios/Pods-buzzvil-sdk-ios-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_buzzvil_sdk_ios : NSObject
+@end
+@implementation PodsDummy_Pods_buzzvil_sdk_ios
+@end
---
+++ buzzvil-sdk-ios/buzzvil-sdk-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  buzzvil-sdk-ios
+//
+//  Created by 주향이 on 10/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
