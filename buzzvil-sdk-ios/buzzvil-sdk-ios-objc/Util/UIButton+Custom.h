95416804566fa813e8c1e9e7304b668b0ae44210
JuHyang <ljh28891004@gmail.com>
2023-11-09 18:10:53 +0900

    update layout on viewController

---
+++ buzzvil-sdk-ios/buzzvil-sdk-ios-objc/Util/UIButton+Custom.h
@@ -0,0 +1,16 @@
+//
+//  UIButton+Custom.h
+//  buzzvil-sdk-ios-objc
+//
+//  Created by 주향이 on 11/9/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIButton_Custom : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ buzzvil-sdk-ios/buzzvil-sdk-ios-objc/Util/UIButton+Custom.m
@@ -0,0 +1,12 @@
+//
+//  UIButton+Custom.m
+//  buzzvil-sdk-ios-objc
+//
+//  Created by 주향이 on 11/9/23.
+//
+
+#import "UIButton+Custom.h"
+
+@implementation UIButton_Custom
+
+@end
