8a4c0b74b216c3a6365d9ae49e7468dadba435e5
Yeji Moon (Nike) <yeaji21@gmail.com>
2024-05-31 08:56:14 +0900

    [OUTPUT-3622] Apply new API: BuzzAdFeed.getBenefitHubFragment() (#225)

--- buzzvil-sdk-android/app/src/main/java/com/buzzvil/sample/buzzvilsdksample/benefithub/BenefitHubFragmentActivity.kt
+++ buzzvil-sdk-android/app/src/main/java/com/buzzvil/sample/buzzvilsdksample/benefithub/BenefitHubFragmentActivity.kt
@@ -2,7 +2,6 @@ package com.buzzvil.sample.buzzvilsdksample.benefithub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.buzzvil.buzzad.benefit.feed.benefithub.BenefitHubFragment
 import com.buzzvil.sample.buzzvilsdksample.R
 import com.buzzvil.sample.buzzvilsdksample.databinding.ActivityBenefitHubFragmentBinding
 
@@ -15,7 +14,12 @@ class BenefitHubFragmentActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         if (savedInstanceState == null) {
-            val benefitHubFragment = BenefitHubFragment.getInstance()
+            // 5.13.x 미만까지 사용하던 방식 (Deprecated)
+            // val benefitHubFragment = BenefitHubFragment.getInstance()
+
+            // 5.13.x 부터는 아래와 같이 사용합니다.
+            val buzzAdFeed = BuzzAdFeed.Builder().build()
+            val benefitHubFragment = buzzAdFeed.getBenefitHubFragment()
 
             supportFragmentManager.beginTransaction().apply {
                 add(R.id.fragmentContainer, benefitHubFragment)
