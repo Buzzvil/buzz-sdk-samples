25f4f862ddb633fec32980def7e3203a57d70953
JuHyang <ljh28891004@gmail.com>
2024-11-05 14:47:25 +0900

    remove cxx debug

--- buzzbooster-react-native-nativemodules/android/app/.cxx/Debug/5m156961/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/fbjni/fbjniConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET fbjni::fbjni)
-add_library(fbjni::fbjni SHARED IMPORTED)
-set_target_properties(fbjni::fbjni PROPERTIES
-    IMPORTED_LOCATION "/Users/juhyang/.gradle/caches/8.10.2/transforms/3edf1a87345f97a0a10d63bef50c344e/transformed/fbjni-0.6.0/prefab/modules/fbjni/libs/android.x86_64/libfbjni.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/juhyang/.gradle/caches/8.10.2/transforms/3edf1a87345f97a0a10d63bef50c344e/transformed/fbjni-0.6.0/prefab/modules/fbjni/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- buzzbooster-react-native-nativemodules/android/app/.cxx/Debug/5m156961/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::libhermes)
-add_library(hermes-engine::libhermes SHARED IMPORTED)
-set_target_properties(hermes-engine::libhermes PROPERTIES
-    IMPORTED_LOCATION "/Users/juhyang/.gradle/caches/8.10.2/transforms/f17d05ab89693f59863034d9dd750308/transformed/hermes-android-0.76.1-debug/prefab/modules/libhermes/libs/android.x86_64/libhermes.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/juhyang/.gradle/caches/8.10.2/transforms/f17d05ab89693f59863034d9dd750308/transformed/hermes-android-0.76.1-debug/prefab/modules/libhermes/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
